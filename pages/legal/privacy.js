b282cb2e71d8388f2322bb0f69c914fa238ef3fb
Dominic Fournier <me@dominicfournier.com>
2021-11-27 14:49:50 -0500

    Initial commit, only contains legal URLs for referencing in Google Consent Screen.

---
+++ pages/legal/privacy.js
@@ -0,0 +1,9 @@
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Privacy Policy</h1>
+      <p>We're currently working on producing our Privacy Policy. As of now, use our service at your own risk, we do not promise that your data will be safe as we are extremely early in development.</p>
+      <p>Thank you for your understanding.</p>
+    </div>
+  )
+}
---
+++ pages/legal/terms.js
@@ -0,0 +1,9 @@
+export default function Terms() {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>We're currently working on producing our Terms and Conditions. As of now, as of now, you shouldn't be using our service and refrain from attempting any kind of attack using bots.</p>
+      <p>Thank you for your understanding.</p>
+    </div>
+  )
+}
