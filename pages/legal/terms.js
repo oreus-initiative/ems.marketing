27f68e02969dcd6c829d8bf7966c9285ed0843eb
Dominic Fournier <me@dominicfournier.com>
2021-11-27 14:54:41 -0500

    Attempt to fix build

--- pages/legal/terms.js
+++ pages/legal/terms.js
@@ -2,7 +2,7 @@ export default function Terms() {
   return (
     <div>
       <h1>Terms and Conditions</h1>
-      <p>We're currently working on producing our Terms and Conditions. As of now, as of now, you shouldn't be using our service and refrain from attempting any kind of attack using bots.</p>
+      <p>We&apos;re currently working on producing our Terms and Conditions. As of now, as of now, you shouldn&apos;t be using our service and refrain from attempting any kind of attack using bots.</p>
       <p>Thank you for your understanding.</p>
     </div>
   )
